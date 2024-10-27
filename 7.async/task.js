b00f2962971b0a7b00d9a52791c518e6ddd54ea4
Zelenkov <sergejselenkow25@gmail.com>
2024-10-27 14:03:20 +0100

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,64 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!callback || !time) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		}
+
+		if (this.alarmCollection.find(alarm => alarm.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
+		this.alarmCollection.push({
+			time,
+			callback,
+			canCall: true,
+		});
+
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		const now = new Date();
+		const houers = String(now.getHours()).padStart(2, "0");
+		const minutes = String(now.getMinutes()).padStart(2, "0");
+		return `${houers}:${minutes}`;
+	}
+
+	start() {
+		if (this.intervalId !== null) {
+			return;
+		}
+
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach(alarm => {
+				if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+					alarm.canCall = false;
+					alarm.callback();
+				}
+			});
+		}, 1000)
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(alarm => {
+			alarm.canCall = true;
+		});
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
